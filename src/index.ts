bcd3c8d9ef0f3c07e45a75ff0a2363efbd28df55
Damien Senger <damien.senger@castoredc.com>
2019-12-02 11:12:05 +0100

    Lint files with new eslint / prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { Command, flags } from "@oclif/command";
+import { Command, flags } from '@oclif/command';
 
 class MonthlyReport extends Command {
   static description =
-    "exports all issues and pull requests closed for the requested month for the requested GitHub repository";
+    'exports all issues and pull requests closed for the requested month for the requested GitHub repository';
 
   static flags = {
-    version: flags.version({ char: "v" }),
-    help: flags.help({ char: "h" }),
-    repository: flags.string({ char: "n", description: "repository" })
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    repository: flags.string({ char: 'n', description: 'repository' }),
   };
 
-  static args = [{ name: "file" }];
+  static args = [{ name: 'file' }];
 
   async run() {
     const { args, flags } = this.parse(MonthlyReport);
